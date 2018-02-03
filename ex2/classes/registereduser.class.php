dcfeb84c94db70a660474769b2e42b59a83b561f
Melanie <35338338+milkum0129@users.noreply.github.com>
2018-02-03 12:06:19 -0500

    Exercise 2 Files
    php files for exercise 2

---
+++ ex2/classes/registereduser.class.php
@@ -0,0 +1,28 @@
+<?php
+
+class registereduser extends users {
+
+
+public function __construct($user_level,$user_id)
+{
+	parent::__construct($user_level);
+	$this->user_id=$user_id;
+	$this->user_level=$user_level;
+}
+
+public function __set($name,$value)
+{
+	$this->$name=$value;
+	return;
+}
+public function __get($name)
+{
+	return $this->$name;
+}
+
+public function __destruct()
+{
+
+}
+}
+?>
