12111178f2f41d642a36c9fc53be7df44b33839d
Melanie <35338338+milkum0129@users.noreply.github.com>
2018-03-18 18:24:36 -0400

    Update A3 Blog File
    update blog file

--- a3/views/blog.php
+++ a3/views/blog.php
@@ -27,7 +27,7 @@ if( is_array($post) ) {
 	<?php foreach($posts as $p){?>
     <h3><a href="<?php echo BASE_URL?>blog/view/<?php echo $p['pID'];?>" title="<?php echo $p['title'];?>"><?php echo $p['title'];?></a></h3>
 	<p><?php echo $p['content'];?></p>
-	<p><?php echo "Posted on " .date("r", strtotime($p['date']));?></p>
+	<p><?php echo "Posted on " .date('Y-m-d H:i:s', strtotime($p['date']));?></p>
 <?php }?>
 </div>
 
